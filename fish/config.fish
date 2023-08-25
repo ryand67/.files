4127c988946c97c6c0752ddafb19b0eeb1882611
Ryan Devenney <rwdevenney@gmail.com>
2023-08-25 09:51:42 -0400

    new shortcut

--- fish/config.fish
+++ fish/config.fish
@@ -17,10 +17,11 @@ alias ll="ls -al"
 alias c="clear"
 alias ch="clear && cd"
 alias cr="cargo run"
+alias gr="go run main.go"
 alias conf="cd ~/.config"
 alias confn="cd ~/.config/nvim"
+alias cdp="cd ~/projects"
 alias cdos="cd ~/projects/open_source"
-alias doom="~/.config/emacs/bin/doom"
 
 alias gs="git status"
 alias ga="git add ."
@@ -45,3 +46,6 @@ end
 function sql
     sqlite3 $argv
 end
+
+# opam configuration
+source /home/rdevenney/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
