15534b97f1bde6575a070935410f1f3c70f53668
Ryan Devenney <rwdevenney@gmail.com>
2024-02-29 15:18:22 -0500

    wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+-- config.color_scheme = 'AdventureTime'
+
+config.tab_bar_at_bottom = true
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+-- and finally, return the configuration to wezterm
+return config
